7224d1d763cb288ee0ec0ec3d8af2d721a5e2470
GanjiAnusha <anoo2k2@gmail.com>
2026-07-08 23:01:03 +0530

    Auto-save

--- 6_Strings.js
+++ 6_Strings.js
@@ -27,7 +27,7 @@ let guestList=`Guests:
 * Bob`;
 console.log(guestList);//allow multiple lines with backticks
 
-//string length is property not a function
+/**string length is property not a function*/
 
 console.log('my \n'.length);//4
 let str="Hello";
@@ -38,7 +38,7 @@ console.log("last character:",str.at(-1));//access a character at certain postit
 
 
 
-//Acessing characters
+/**Acessing characters*/
 
 console.log("first character of 'Hello':", 'Hello'[0]); // "H"
 console.log("second character of 'Hello':", 'Hello'[1]); // "e"
@@ -47,15 +47,48 @@ console.log("fourth character of 'Hello':", 'Hello'[3]); // "l"
 console.log("fifth character of 'Hello':", 'Hello'[4]); // "o"
 
 
+/**strings are immutable in js */
+
 let str1='Hi';
 // str[0] ='n';
 console.log(str1[0]);//doesn't work error why because strings are immmutable in js
 
-//changing case
+/**changing case*/
 
 console.log('Hello'.toUpperCase()); // "HELLO"
 console.log('Hello'.toLowerCase()); // "hello"  
-console.log('Hello'[0].toLocaleLowerCase());//Uses locale-specific case mappings based on the environment. like turkish,....
-console.log('Hello'[0].toLocaleUpperCase());
+//Uses locale-specific case mappings based on the environment. like turkish,....
+console.log('Hello'[0].toLocaleLowerCase());//h
+console.log('Hello'[0].toLocaleUpperCase());//H
+console.log('Hello'[2].toUpperCase());//L changes single char at specific pos
+
+
+
+/**Searching substring.i.e.., str.indexOf(substr,pos),it returns -1 when match is found */
+
+let str2='Widget with id';
+console.log(str2.indexOf('Widget'))//0,because found at begining
+console.log(str2.indexOf('widget'))//-1,because found at begining,but case-senstive
+console.log(str2.indexOf("id"))//1,because found case at postition 1
+console.log(str2.indexOf('id',2))//12 It will search for next occurence from position 2
+
+let str3="As sly as a fox,as strong as an fox";
+let target="as";//search
+
+let pos=0;
+while(true){
+    let foundpos=str3.indexOf(target,pos);
+    if(foundpos==-1)
+    break;
+    console.log(`found at ${foundpos}`);//found at 7,17,27
+    pos=foundpos+1;
+}
+
+
+//same code as above but shorter version
+let pos1 = -1;
+while ((pos1 = str.indexOf(target, pos1 + 1)) != -1) {
+    console.log(pos1);//7,17,27
+}
 
 
