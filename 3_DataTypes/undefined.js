62b452b1f94fde5ca3800880ad7d674cca01db10
GanjiAnusha <anoo2k2@gmail.com>
2026-07-12 16:29:16 +0530

    Auto-save

--- 3_DataTypes/undefined.js
+++ 3_DataTypes/undefined.js
@@ -1,4 +1,9 @@
 /**
 ➡️Undefined is a placehholder before code is executed.It is not same as notdefined
+➡️The variable is known, but it is empty,whereas,The variable is completely unknown or inaccessible is reference error
+➡️code runs normally when variable is undefined,it is primitive datatype
  */
-console.log()
+console.log(a);//undefined
+var a=10;//undefined
+console.log(a);//10
+console.log(x);//not defined --reference error
